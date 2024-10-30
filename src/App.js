79bf1efc67256b1a1018f7010718a67e5a51ba19
juwon-ace <adexasquare@gmail.com>
2024-10-30 11:25:16 +0100

    kuda

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Home from "../src/pages/Home"
+import Header from "../src/Static/Header"; 
+import Footer from "../src/Static/Footer"
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Home/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
+
+
